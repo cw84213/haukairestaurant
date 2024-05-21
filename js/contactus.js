7af9db46cc40f61d879626a1f190fe0af5522291
cw84213 <165855710+cw84213@users.noreply.github.com>
2024-05-21 18:31:03 +1200

    contactus js

---
+++ js/contactus.js
@@ -0,0 +1,15 @@
+function showMessage() {
+    // Collecting form field values
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;    
+    var message = document.getElementById("message").value;
+    
+    // Constructing the confirmation message
+    var confirmationMessage = "Your message has been sent successfully!\n\n";
+    confirmationMessage += "Name: " + name + "\n";
+    confirmationMessage += "Email: " + email + "\n";   
+    confirmationMessage += "Message: " + message + "\n";
+    
+    // Displaying the confirmation message
+    alert(confirmationMessage);
+}
