7aeb3d80cff8d792059b70d65365f74ed6f381db
cw84213 <165855710+cw84213@users.noreply.github.com>
2024-05-21 18:31:26 +1200

    login js

---
+++ js/login.js
@@ -0,0 +1,67 @@
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Authentication process
+    if (authenticate(username, password)) {
+        document.getElementById("login-error").style.display = "none";
+        displayGoogleCalendar();
+    } else {
+        document.getElementById("login-error").style.display = "block";
+        clearCalendar();
+        hideShiftSwapButton();
+    }
+}
+
+function authenticate(username, password) {    
+    return username === 'haukaiemployees' && password === 'havealovelyday';
+}
+
+function displayGoogleCalendar() {
+    // Display Google Calendar using an iframe
+    var calendarFrame = document.createElement('iframe');
+    calendarFrame.src = "https://calendar.google.com/calendar/embed?src=thehaukairestaurantbusiness%40gmail.com&ctz=Pacific%2FAuckland&mode=WEEK";
+    calendarFrame.width = '100%'; 
+    calendarFrame.height = '600';
+    calendarFrame.className = 'calendar-iframe'; 
+    calendarFrame.frameborder = '0';
+    calendarFrame.scrolling = 'no';
+
+    clearCalendar(); 
+    document.getElementById("calendar-container").appendChild(calendarFrame);
+
+    // Create the button below the calendar
+    showShiftSwapButton();
+}
+
+function clearCalendar() {
+    document.getElementById("calendar-container").innerHTML = ""; 
+}
+
+function showShiftSwapButton() {
+    var shiftSwapButton = document.createElement('button');
+    shiftSwapButton.textContent = "Request Shift Swap";
+    shiftSwapButton.onclick = function() {
+        document.getElementById("shift-swap-request").style.display = "block";
+    };
+    shiftSwapButton.id = "request-shift-swap-btn";
+    shiftSwapButton.className = "request-shift-swap-button"; 
+    var calendarContainer = document.getElementById("calendar-container");
+    calendarContainer.parentNode.insertBefore(shiftSwapButton, calendarContainer.nextSibling);
+}
+
+function hideShiftSwapButton() {
+    var shiftSwapButton = document.getElementById('request-shift-swap-btn');
+    if (shiftSwapButton) {
+        shiftSwapButton.parentNode.removeChild(shiftSwapButton);
+    }
+}
+
+function submitShiftSwapRequest() {
+    displayConfirmationMessage();
+}
+
+function displayConfirmationMessage() {
+    // Create a pop-up message to inform the user that the request has been sent
+    alert("Your shift swap request has been sent to the manager for approval.");
+}
